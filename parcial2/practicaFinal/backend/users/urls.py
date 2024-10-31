2087db98dc0425e49ce50854cf0a91badb7cb196
OrozcoFidel4 <fidel21304022@gmail.com>
2024-10-30 23:03:49 -0700

    Practica Final Completada

---
+++ parcial2/practicaFinal/backend/users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from rest_framework import routers
+from users import views
+
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserApiViewSet)
+
+urlpatterns=[
+    path('', include(router.urls)),
+    path('auth/me/', views.UserView.as_view()),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Aqui pueden utilizar el nombre de auth/token/  o  auth/login/
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
